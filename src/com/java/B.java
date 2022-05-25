bf7e227ddfcf55adf22f1a7616bce693dfeeeaec
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-26 00:22:20 +0530

    Add Two Number Program

---
+++ src/com/java/B.java
@@ -0,0 +1,10 @@
+// Add Two Numbers Program
+
+package com.java;
+
+public class B {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Add:-" + (a + b));
+    }
+}
