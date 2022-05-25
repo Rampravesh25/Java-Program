6f7f2a742ead35a9dc3bc1b4d1fedf67fb59ab17
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-26 00:29:44 +0530

    How to take input from User Program

---
+++ src/com/java/C.java
@@ -0,0 +1,15 @@
+// How to take input from User
+
+package com.java;
+
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        System.out.println("PLease Enter The Value:-");
+        int n;
+        Scanner scan=new Scanner(System.in);
+        n=scan.nextInt();
+        System.out.println("Enter Value Is This:-"+" "+n);
+    }
+}
