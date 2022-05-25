4b816fc9f4dac53c1cc29368261b4dd48e74d19d
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-26 01:16:48 +0530

    Print Even number in Given Range

---
+++ src/com/java/I.java
@@ -0,0 +1,21 @@
+/*Print Odd Even in Given Range*/
+
+package com.java;
+
+import java.util.Scanner;
+
+public class I {
+    public static void main(String[] args) {
+        System.out.println("Please Enter the Number:- ");
+        Scanner scan = new Scanner(System.in);
+        int val1 = scan.nextInt();
+
+        for (int i = 2; i < val1; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
